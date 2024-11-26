cace79b0eaa009e459db71e5805ffc5e86df1276
Swann <swann.rouanet@gmail.com>
2024-11-26 13:55:52 +0100

    feat: make username required

--- src/commands/holmes.ts
+++ src/commands/holmes.ts
@@ -14,6 +14,7 @@ export const data: CommandData = {
       type: ApplicationCommandOptionType.String,
       name: "username",
       description: "The username to search for",
+      required: true,
     }
   ]
 }
