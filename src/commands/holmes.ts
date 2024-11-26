a657282fb7b0f455a3828ac74393021857ab900d
Swann <swann.rouanet@gmail.com>
2024-11-26 12:48:59 +0100

    feat: create holmes command to search username on services

---
+++ src/commands/holmes.ts
@@ -0,0 +1,48 @@
+import type { CommandData, SlashCommandProps, CommandOptions  } from "commandkit";
+import {
+  ApplicationCommandOptionType,
+} from "discord.js";
+
+import { checkUsername } from "../lib/fetcher/fetcher";
+import services from "../lib/fetcher/services.json";
+
+export const data: CommandData = {
+  name: "holmes",
+  description: "Search a username across various services",
+  options: [
+    {
+      type: ApplicationCommandOptionType.String,
+      name: "username",
+      description: "The username to search for",
+    }
+  ]
+}
+
+export const options: CommandOptions = {
+  devOnly: false,
+  deleted: false,
+}
+
+export async function run({ interaction, client }: SlashCommandProps) {
+  const username = interaction.options.getString("username");
+
+  if (!username) {
+    return interaction.reply("Please provide a username to search for");
+  }
+
+  await interaction.deferReply(); // Acknowledge the interaction
+
+  const results = await Promise.all(
+    services.map(service => checkUsername(service, username))
+  )
+
+  const response = results
+    .filter(result => result.exist)
+    .map(
+      (result) =>
+        `**${result.service}**: <${result.url}>`
+    )
+    .join("\n");
+
+  interaction.editReply(response);
+}
---
+++ src/lib/fetcher/fetcher.ts
@@ -0,0 +1,32 @@
+interface Service {
+  name: string,
+  url: string,
+  existsWhen: number,
+}
+
+export async function checkUsername(service: Service, username: string): Promise<{ service: string; exist: boolean; url: string }> {
+  const url = service.url.replace("{username}", username);
+
+  const controller = new AbortController();
+  const timeout = setTimeout(() => controller.abort(), 5000); // 5 seconds
+
+  try {
+    const response = await fetch(url, { method: "HEAD", signal: controller.signal });
+    return {
+      service: service.name,
+      exist: response.status === service.existsWhen,
+      url,
+    };
+  } catch (error) {
+    if (error instanceof Error && error.name === "AbortError") {
+      console.error(`Request to ${url} timed out.`);
+    }
+    return {
+      service: service.name,
+      exist: false,
+      url,
+    };
+  } finally {
+    clearTimeout(timeout);
+  }
+}
